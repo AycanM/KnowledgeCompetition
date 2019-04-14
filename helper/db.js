044a6ba54797db3bb0c1f95acec701123627dbd6
Aycan Mintas <aycanmintas34@gmail.com>
2019-04-14 16:48:32 +0300

    repair database connectionÃ

--- helper/db.js
+++ helper/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/knowledge-competition', { useMongoClient: true});
+    mongoose.connect('mongodb://localhost/knowledge-competition', { useNewUrlParser:true });
   
     mongoose.connection.on('open', () => {
        console.log('MongoDB: Connected');
