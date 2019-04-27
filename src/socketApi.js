58c24d95a727dfcb3347eb76e0809c217dd7034e
Aycan Mintas <aycanmintas34@gmail.com>
2019-04-28 00:42:41 +0300

    code review

--- src/socketApi.js
+++ src/socketApi.js
@@ -3,41 +3,74 @@ const uuid = require('uuid');
 const socketApi = {};
 socketApi.io = io;
 
-
 const rooms = {};
-let roomName = uuid();
-
-
 
 io.on('connection', socket => {
-    socket.on("joinRoom", data => {
-        let newUser = {
-            id:socket.id,
-            name:data.name,
-        };
-        
-        if(rooms[roomName] === undefined){
-                rooms[roomName] = {
-                users:[]
-            };
-            rooms[roomName].users.push(newUser);
+    socket.on("joinRoom", ({name}) => {
+        let newUser = { 
+            id : socket.id, 
+            name,
         }
-        else if(rooms[roomName].users !== undefined && rooms[roomName].users.length < 2){
-            rooms[roomName].users.push(newUser);
+
+        let room = GetAvailableRoom();
+
+        if(room === null || room === undefined){
+            room = CreateRoom();
         }
-        else{
-            roomName = uuid();
-            rooms[roomName] = {
-                users:[]
-            };
-            rooms[roomName].users.push(newUser);
-        }
-        socket.join(roomName, data => {
-            console.log(`${roomName} odasına bağlanıldı`);
-            io.to(roomName).emit("newQuestion", {question:`Test question for ${roomName}`});
-        });
+        console.log(room);
+
+        AddUserToRoom(newUser, room);
+
     });
 });
 
+const GetAvailableRoom = () => {
+    for(let room in rooms){
+        console.log(room);
+        if(room !== undefined && room.users !== undefined && room.users.length < 2){
+            return room;
+        }
+    }    
+};
+
+const CreateRoom = () => {
+    let room = uuid();
+    rooms[room] = {
+        users:[]
+    };
+    return room;
+};
+
+const AddUserToRoom = (user, roomName) => {
+    rooms[roomName].users.push(user);
+};
+
 
 module.exports = socketApi;
+
+
+// let newUser = {
+        //     id:socket.id,
+        //     name:data.name,
+        // };
+        
+        // if(rooms[roomName] === undefined){
+        //         rooms[roomName] = {
+        //         users:[]
+        //     };
+        //     rooms[roomName].users.push(newUser);
+        // }
+        // else if(rooms[roomName].users !== undefined && rooms[roomName].users.length < 2){
+        //     rooms[roomName].users.push(newUser);
+        // }
+        // else{
+        //     roomName = uuid();
+        //     rooms[roomName] = {
+        //         users:[]
+        //     };
+        //     rooms[roomName].users.push(newUser);
+        // }
+        // socket.join(roomName, data => {
+        //     console.log(`${roomName} odasına bağlanıldı`);
+        //     io.to(roomName).emit("newQuestion", {question:`Test question for ${roomName}`});
+        // });
