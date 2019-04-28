d15add1f5195ab7df51c2fc73d6458a70f781fe0
Aycan Mintas <aycanmintas34@gmail.com>
2019-04-28 16:28:17 +0300

    Create game infrastructure

--- public/javascripts/competition.js
+++ public/javascripts/competition.js
@@ -1,8 +1,76 @@
-var socket = io();
-
+const socket = io();
+let questionNumber = 0;
+let roomName_Client = null;
+let trueOption = null;
+let clickedAnyOpt = false;
 $(document).ready(() =>{
-    socket.emit("joinRoom", {name:"Oğuzhan"});
-    socket.on('hello', ({message}) => {
-        console.log(message);
+
+    let userName = prompt("Lütfen kullanıcı adı giriniz");
+    if(userName.trim() === '' || userName === null || userName === undefined)
+        userName = prompt("Lütfen kullanıcı adı giriniz");
+
+
+    
+    $("#option-section").hide();
+    $("#question-header").hide();
+    $("#players").hide();
+    
+    socket.emit('JoinRoom', userName, roomName => {
+        roomName_Client = roomName;
     });
+
+    socket.on('NotEnoughUser', ( { message }) =>{
+        $("#question").html("Yarışmanın başlaması için minimum kullanıcı sayısı sağlanmış değil.Lütfen bekleyiniz");            
+    });
+    socket.on('ReadyForGame', ( { message }) =>{
+        $("#question").html("Kısa süre içerisinde yarışmamız başlayacak.")
+        setTimeout(() => {
+            socket.emit('StartGame');
+        }, 1000);
+    });
+
+    socket.on('UsersInRoom', users => {
+        $("#players").show();
+        for (let i = 0; i < users.length; i++) {
+            $('#p' + (i+1)).html(users[i].name + '<br><span id="pt'+ (i+1) + '">(' + users[i].point + ')</span>');
+        }
+    });
+
+    socket.on('SendQuestion', (question) => {
+        $("#question-header").show();
+        $("#option-section").show();
+        $("#question").html(question.text);
+        $("#option-a-txt").html(question.a);
+        $("#option-b-txt").html(question.b);
+        $("#option-c-txt").html(question.c);
+        $("#option-d-txt").html(question.d);
+        trueOption = question.trueOption;
+        questionNumber++;
+    });
+
+    $('.opt').click((e)=>{
+
+        if(!clickedAnyOpt){
+            let clickedOptionId = e.target.id;
+            let clickedOption = clickedOptionId.split("-"); 
+            if(clickedOption[1] === trueOption){
+                if(clickedOption.length === 2)
+                    $('#' + clickedOptionId).css('background-color', 'green');
+                else
+                    $('#opt-' + trueOption).css('background-color', 'green');
+            }
+            else{
+                if(clickedOption.length === 2)
+                    $('#' + clickedOptionId).css('background-color', 'red');
+                else
+                    $('#opt-' + clickedOption[1]).css('background-color', 'red');
+            }
+            clickedAnyOpt = true;
+        }else{
+            return false;
+        }
+
+            
+    });
+    
 });
--- src/socketApi.js
+++ src/socketApi.js
@@ -6,53 +6,106 @@ socketApi.io = io;
 const rooms = {};
 
 io.on('connection', socket => {
-    socket.on("joinRoom", ({name}) => {
+    socket.on('JoinRoom', (name, callback) => {
         let newUser = { 
             id : socket.id, 
             name,
+            point:0,
         }
         let room = JoinRoomSwitcher();
         AddUserToRoom(newUser, room);
         socket.join(room, ()=>{
+            callback(room);
             io.to(room).emit('hello', {message: `Hello ${room}`});
+            if(rooms[room].users.length === 2){
+                io.to(room).emit('ReadyForGame', { message : 'Ready'});
+            }
+            else{
+                io.to(room).emit('NotEnoughUser', { message : `Not enough user in this room: ${room}`});
+            }
+            socket.on('StartGame', () =>{
+                io.to(room).emit('UsersInRoom', rooms[room].users);
+                io.to(room).emit('SendQuestion', {
+                        text:'Test question',
+                        a : 'A option',
+                        b : 'B option',
+                        c : 'C option',
+                        d : 'D option',
+                        trueOption: 'a',
+                    } 
+                );
+            });
         });
+    });
+    socket.on('disconnect', () => {
+        RemoveUserFromRoom(socket.id);
     });
 });
 
 const GetAvailableRoom = () => {
     console.log("LOG: GET_AVAILABLE_ROOM");
     for(let room in rooms){
-        console.log(room);
+        console.log('get_room_t: ' + room)
         console.log(rooms[room].users);
-        if(room !== null && room !== undefined && rooms[room].users !== undefined && rooms[room].users.length < 3){
-            console.log("test");
+        if(room !== null && room !== undefined && rooms[room].users !== undefined && rooms[room].users.length < 2){
+            console.log('get_room_t: ' + room)
             return room;
         }
-    }    
+    }
+    console.log("-------------------------");
+
 };
 
 const CreateRoom = () => {
     console.log("LOG: CREATE_ROOM");
     let room = uuid();
     rooms[room] = {
-        users:[]
+        users:[],
+        currentQuestion : -1,
     };
+    console.log('create_room: ' + room);
+    console.log("-----------------");
     return room;
 };
 
 const AddUserToRoom = (user, roomName) => {
     console.log("LOG: ADD_USER_TO_ROOM");
     rooms[roomName].users.push(user);
+    console.log(rooms[roomName].users);
+    console.log("----------------------");
 };
 
 
 const JoinRoomSwitcher = () => {
     console.log("LOG: JOIN ROOM SWITCHER");
     let room = GetAvailableRoom();
+    console.log('swtchr_room_1: '  + room);
     if(room === null || room === undefined){
         room = CreateRoom();
     }
+    console.log('swtchr_room_2: '  + room);
+    console.log("------------------------");
     return room;
-}
+};
+
+const RemoveUserFromRoom = (socketId) => {
+    for (const room in rooms) {
+        let users = rooms[room].users;
+        for (let i = 0; i < users.length; i++) {
+              if(users[i].id === socketId){
+                  users.splice(i, 1);
+                  if(users.length === 0) // Oda da herhangi bir kullanıcı kalmadığında odayı sil.
+                  DeleteRoom(room);
+              }
+              
+              
+        }
+    }
+};
+
+const DeleteRoom = (room) =>{
+    delete rooms[room];
+};
+
 
 module.exports = socketApi;
